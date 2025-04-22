1f2f673aebf0628b04984cd74610a12cb69b96cd
inkhemi <claudiorh1953@gmail.com>
2025-04-22 14:21:47 -0400

    Added Buttons to Add and Modify Products buttons are just placeholders for now

--- src/pages/admin_products.tsx
+++ src/pages/admin_products.tsx
@@ -9,22 +9,39 @@ const AdminProducts: React.FC = () => {
     { id: 5, name: "Salsa de Soya", quantity: 10, unit: "litros" },
   ]
 
+  const handleAddIngredient = () => {
+    alert("Agregar ingrediente")
+  }
+
+  const handleModifyQuantity = (id: number) => {
+    alert(`Modificar cantidad del ingrediente con ID: ${id}`)
+  }
+
   return (
     <div className='p-6 bg-gray-100 min-h-screen'>
       <h1 className='text-2xl font-bold mb-4'>Inventario de ingredientes</h1>
+      <div className='mb-4 flex gap-4'>
+        <button
+          onClick={handleAddIngredient}
+          className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'
+        >
+          Agregar Ingrediente
+        </button>
+      </div>
       <div className='overflow-x-auto'>
         <table className='min-w-full bg-white border border-gray-200'>
           <thead>
             <tr>
               <th className='px-4 py-2 border-b'>ID</th>
-              <th className='px-4 py-2 border-b'>ingrediente</th>
+              <th className='px-4 py-2 border-b'>Ingrediente</th>
               <th className='px-4 py-2 border-b'>Cantidad</th>
               <th className='px-4 py-2 border-b'>Unidad</th>
+              <th className='px-2 py-1 border-b'>Acciones</th>
             </tr>
           </thead>
           <tbody>
             {materials.map((material) => (
-              <tr key={material.id} className='hover:bg-gray-100'>
+              <tr key={material.id} className='hover:bg-gray-400'>
                 <td className='px-4 py-2 border-b text-center'>
                   {material.id}
                 </td>
@@ -34,6 +51,16 @@ const AdminProducts: React.FC = () => {
                 </td>
                 <td className='px-4 py-2 border-b text-center'>
                   {material.unit}
+                </td>
+                <td className='px-4 py-2 border-b text-center'>
+                  <div className='flex justify-center items-center gap-2'>
+                    <button
+                      onClick={() => handleModifyQuantity(material.id)}
+                      className='p-1 text-white bg-green-700 hover:bg-green-800 text-sm rounded'
+                    >
+                      Editar
+                    </button>
+                  </div>
                 </td>
               </tr>
             ))}
